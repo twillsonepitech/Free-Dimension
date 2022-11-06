898d73c06c29b900e6d68618e502dfca7e59086d
Thomas Willson <thomas.willson@epitech.eu>
2022-11-06 15:10:19 +0100

    mod: norme

--- free.c
+++ free.c
@@ -31,30 +31,30 @@ void *__get_next_dimension_ptr(const void * __restrict__ __data)
 
 void __free_ptr(void * __restrict__ __data, unsigned int __dimension, unsigned int __current, size_t * __restrict__ __nmemb, __next_dimension_fn_t __next_dim)
 {
-    size_t __i;
+    size_t i;
 
     if (__dimension == __current + 1)
     {
         free((char **)__data);
         return;
     }
-    __i = __INIT_INTEGER;
-    while (__i < __nmemb[__current])
+    i = __INIT_INTEGER;
+    while (i < __nmemb[__current])
     {
-        __free_ptr(__next_dim((char **)__data + __i), __dimension, __current + 1, __nmemb, __next_dim);
-        __i++;
+        __free_ptr(__next_dim((char **)__data + i), __dimension, __current + 1, __nmemb, __next_dim);
+        i++;
     }
     free((char **)__data);
 }
 
 int __free(void * __restrict__ __data, unsigned int __flags)
 {
-    __nmemb_fn_t __nmemb;
-    size_t __i;
-    unsigned int __dimension;
+    __nmemb_fn_t nmemb;
+    size_t i;
+    unsigned int dimension;
 
     if (__flags < (__S_IFSPL) || __flags > (__S_IFSPL | __S_IFDBL | __S_IFTPL)    \
-        || NULL == memset(&__nmemb, __INIT_STRUCT, sizeof(__nmemb_fn_t)))
+        || NULL == memset(&nmemb, __INIT_STRUCT, sizeof(__nmemb_fn_t)))
     {
         return EXIT_FAILURE;
     }
@@ -67,16 +67,16 @@ int __free(void * __restrict__ __data, unsigned int __flags)
         { .__minimum = (__S_IFDBL), .__maximum = (__S_IFSPL | __S_IFDBL),             .__dimension = __S_ISDBL },
         { .__minimum = (__S_IFTPL), .__maximum = (__S_IFSPL | __S_IFDBL | __S_IFTPL), .__dimension = __S_ISTPL },
     };
-    __i = __INIT_INTEGER;
-    while (__i < sizeof(__get_dimension) / sizeof(__get_dimension[0]))
+    i = __INIT_INTEGER;
+    while (i < sizeof(__get_dimension) / sizeof(__get_dimension[0]))
     {
-        if (__flags >= __get_dimension[__i].__minimum && __flags <= __get_dimension[__i].__maximum)
+        if (__flags >= __get_dimension[i].__minimum && __flags <= __get_dimension[i].__maximum)
         {
-            __dimension = __get_dimension[__i].__dimension;
+            dimension = __get_dimension[i].__dimension;
         }
-        __i++;
+        i++;
     }
-    __get_dimension_nmemb_ptr(__data, __dimension, &__nmemb);
-    __free_ptr(__data, __dimension, __INIT_DIMENSION, (size_t[2]){__nmemb.__nmemb1d, __nmemb.__nmemb2d}, &__get_next_dimension_ptr);
+    __get_dimension_nmemb_ptr(__data, dimension, &nmemb);
+    __free_ptr(__data, dimension, __INIT_DIMENSION, (size_t[2]){nmemb.__nmemb1d, nmemb.__nmemb2d}, &__get_next_dimension_ptr);
     return EXIT_SUCCESS;
 }
